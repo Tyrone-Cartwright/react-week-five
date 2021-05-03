c32e37423cdc6c3255307efb47acbba1fe21b225
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2021-05-03 17:37:34 -0400

    react app complete

--- README.md
+++ README.md
@@ -68,3 +68,39 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `yarn build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Primary
+
+- Red: hsl(0, 100%, 74%)
+- Green: hsl(154, 59%, 51%)
+
+### Accent
+
+- Blue: hsl(248, 32%, 49%)
+
+### Neutral
+
+- Dark Blue: hsl(249, 10%, 26%)
+- Grayish Blue: hsl(246, 25%, 77%)
+
+## Typography
+
+### Body Copy
+
+- Font size: 16px
+
+### Font
+
+- Family: [Poppins](https://fonts.google.com/specimen/Poppins)
+- Weights: 400, 500, 600, 700
--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main className="grid">
+        {/* <img src={desktop} alt="desktop print background"/> */}
+        <div className="content">
+          <div className="code">
+            <h1>Learn to code by watching others</h1>
+          </div>
+          <div className="desc">
+            <p>
+              See how experienced developers solve problems in real time. Watching scripted tutorials is great, but understanding how developers think is invaluable.
+            </p>
+          </div>
+        </div>
+        
+
+        <div className="free">
+          <a href="https://wwww.google.com">
+            <h4>Try it free 7 days <span>then $20/mo. thereafter</span></h4>
+          </a>
+        </div>
+        <div className="form">
+          <form className="field">
+            <input type="text" name="firstName" id="fname" placeholder="First Name" required/>
+            <input type="text" name="lastName" id="lname" placeholder="Last Name" required/>
+            <input type="email" name="email" id="email" placeholder="Email Address" required/>
+            <input type="password" name="password" id="pword" placeholder="Password" required/>
+            <a href="https://www.google.com" className="claim">
+              Claim your free trial
+            </a>
+            <p>By clicking the button you are agreeing to our <span className="terms">Terms and Services</span>
+            </p>
+          </form>
+        </div>
+      </main>     
+    </>
   );
 }
 
